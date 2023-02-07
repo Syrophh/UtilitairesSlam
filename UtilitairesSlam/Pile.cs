ce3af85f2271e17dbe379ee5975f266ec2e85c94
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-02-07 15:26:53 +0100

    Ajoutez des fichiers projet.

---
+++ UtilitairesSlam/Pile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MesOutils
+{
+    /// <summary>
+    /// Pile contenant pouvant contenir des entiers.
+    /// On ajoute après le dernier élément ajouté
+    /// On retire toujours le dernier élément ajouté
+    /// </summary>
+    public class Pile<T>
+    {
+        /// <summary>
+        /// Liste contenant les éléments de la pile
+        /// </summary>
+        private List<T> elements;
+
+        /// Property
+        public int Count { get => this.elements.Count; }
+
+        /// <summary>
+        /// Constructeur de la classe <see cref="Pile"/>.
+        /// </summary>
+        /// <param name="nbMaxElt">Taille maximum de la pile</param>
+        public Pile()
+        {
+            this.elements = new List<T>();
+        }
+
+        /// <summary>
+        /// retourne un booléen indiquant si la pile est vide.
+        /// Une pile est vide si le nombre d'élément 
+        /// contenue dans tabElem est égale à 0
+        /// </summary>
+        /// <returns></returns>
+        public bool PileVide()
+        {
+            return this.elements.Count == 0;
+        }
+
+        /// <summary>
+        /// Cette méthode ajoute la valeur passée en paramètre au sommet de la pile
+        /// Si la pile n'est pas pleine.
+        /// Si la pile est pleine, déclenchement d'une exception.
+        /// </summary>
+        /// <param name="valeur">élément à empiler</param>
+        public void Empiler(T valeur)
+        {
+           this.elements.Add(valeur);
+        }
+
+        /// <summary>
+        /// Renvoie la valeur située au sommet de la pile
+        /// Si la pile est vide, la méthode déclenche une Exception.
+        /// </summary>
+        /// <param name="unePile">Pile sur laquelle il faut depiler</param>
+        /// <returns>Valeur dépilée</returns>
+        public T Depiler()
+        {
+            if (PileVide())
+            {
+                throw new Exception("[Erreur] Pile vide, impossible de dépiler un élément");
+            }
+            else
+            {
+                T valeur = this.elements[this.elements.Count - 1];
+                this.elements.RemoveAt(this.elements.Count - 1);
+                return valeur;
+            }
+        }
+    }
+}
